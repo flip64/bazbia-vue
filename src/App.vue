81839968da2e7e83372a5e3dc39639d5a3e4350e
flip64 <65002468+flip64@users.noreply.github.com>
2026-02-15 01:37:45 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@
 
 <script setup lang="ts">
 import { reactive, ref } from "vue"
-import Header from "@/components/layout/Header/Header.vue"
+import Header from "@/components/layout/header/Header.vue"
 import Footer from "@/components/layout/Footer.vue"
 
 /* ---------------- LOADER ---------------- */
@@ -75,8 +75,7 @@ function showToast(msg: string) {
 }
 
 /* ---------------- MODAL ---------------- */
-const modal = reactive({
-  show: false,
+const modal = reactive({h  show: false,
   title: "",
   content: ""
 })

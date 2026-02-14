6ea354dccf3ea434d393e0b8a90603037b8a9df7
flip64 <65002468+flip64@users.noreply.github.com>
2026-02-15 01:26:40 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@
 
 <script setup lang="ts">
 import { reactive } from "vue"
-import Header from "@/components/layout/Header.vue"
+import Header from "@/components/layout/header/Header.vue"
 import Footer from "@/components/layout/Footer.vue"
 
 /* ---------------- LOADER ---------------- */

43a3e10386dc932de6a4eab4a6d518ac9a8072c0
flip64 <65002468+flip64@users.noreply.github.com>
2026-02-14 10:08:20 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,23 @@ import { registerPlugins } from "@/core/plugins"
 
 import { useAuthStore } from "@/modules/auth/store/authStore"
 
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+
 const auth = useAuthStore()
 if (auth.token)
   auth.fetchMe()
 
+app.mount("#app")
 
 
-const app = createApp(App)
 
-app.use(router)
-app.use(store)
+
+
 
 registerPlugins(app)
 
-app.mount("#app")
+

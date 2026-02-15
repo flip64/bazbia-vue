f56a0db53ec3b31992c1cd0b8d5b0b5a35ab7eb1
flip64 <65002468+flip64@users.noreply.github.com>
2026-02-15 19:39:45 +0330

    Update product.types.ts

--- src/types/product.types.ts
+++ src/types/product.types.ts
@@ -1,28 +1,50 @@
-// src/types/product.types.ts
-export interface Product {
+export interface Category {
   id: number
   name: string
   slug: string
-  price: number
-  discount_price: number | null
-  category: string
-  thumb: string
-  variants: any | null
+  image: string
+  subcategories: any[]
+}
+
+export interface Specification {
+  name: string
+  value: string
+}
+
+export interface Variant {
+  id: number
+  sku: string
+  price: string
+  discount_price: string | null
+  stock: number
+  low_stock_threshold: number
+  expiration_date: string | null
+  attributes: any[]
+}
+
+export interface ProductImage {
+  image: string
+  source_url?: string
+  alt_text: string | null
+  is_main: boolean
+}
+
+export interface ProductDetail {
+  id: number
+  name: string
+  slug: string
+  description: string
+  base_price: string
+  category: Category
+  tags: string[]
+  specifications: Specification[]
+  variants: Variant[]
+  images: ProductImage[]
+  videos: any[]
+  is_active: boolean
   created_at: string
-}
-
-export interface ProductsResponse {
-  current_page: number
-  total_pages: number
-  count: number
-  next: string | null
-  previous: string | null
-  data: Product[]
-}
-
-export interface ProductsQueryParams {
-  page?: number
-  category?: string
-  search?: string
-  ordering?: string
-}
+  updated_at: string
+  is_special: boolean
+  special_details: any | null
+  quantity: number
+  }

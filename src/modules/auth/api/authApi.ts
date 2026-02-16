82752b37774f5b6feb51531bd1271df4b02d8e7a
flip64 <65002468+flip64@users.noreply.github.com>
2026-02-16 22:34:40 +0330

    Update authApi.ts

--- src/modules/auth/api/authApi.ts
+++ src/modules/auth/api/authApi.ts
@@ -1,15 +1,10 @@
-import { api } from "@/core/api/client"
+import api from '@/core/api/client'
 
-export const loginRequest = (data:{email:string,password:string}) =>
-  api.post("/auth/login", data)
+export const loginRequest = (data: {email: string, password: string}) =>
+  api.post('/auth/login', data)
 
-export const registerRequest = (data:{name:string,email:string,password:string}) =>
-  api.post("/auth/register", data)
+export const registerRequest = (data: {name: string, email: string, password: string}) =>
+  api.post('/auth/register', data)
 
 export const getMeRequest = () =>
-  api.get("/auth/me")
-
-
-
-
-
+  api.get('/auth/me')

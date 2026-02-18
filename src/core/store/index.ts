0624c1716fc29ba3ba678a27b932adfce0881739
flip64 <65002468+flip64@users.noreply.github.com>
2026-02-18 07:14:26 +0330

    Update index.ts

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,2 +1,7 @@
-import { createPinia } from "pinia"
+// src/core/store/index.ts
+import { createPinia } from 'pinia'
 export const store = createPinia()
+
+export { useAuthStore } from './authStore'
+export { useCartStore } from './cartStore'
+// export { useWishlistStore } from './wishlistStore'

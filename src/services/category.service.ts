bddb1c4fbda8e79850c7c2cf61a4b920f8f0c128
flip64 <65002468+flip64@users.noreply.github.com>
2026-06-18 15:15:43 +0330

    Update category.service.ts

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,4 +1,4 @@
-import { apiClient } from '@/core/api/client'
+import  apiClient  from '@/core/api/client'
 import type { Category } from '@/types/category.types'
 
 export const categoryService = {

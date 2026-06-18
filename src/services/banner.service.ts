752999b1ae58ee29927f5a88e9868c5f68017b19
flip64 <65002468+flip64@users.noreply.github.com>
2026-06-18 14:45:12 +0330

    Create banner.service.ts

---
+++ src/services/banner.service.ts
@@ -0,0 +1,8 @@
+import { apiClient } from '@/core/api/client'
+
+export const bannerService = {
+  async getBanners() {
+    const res = await apiClient.get('/promotions/banners/')
+    return res.data
+  }
+}

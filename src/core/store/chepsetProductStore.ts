745d1b0d448459a78bbca48fc89e09cc5120c9be
flip64 <65002468+flip64@users.noreply.github.com>
2026-06-23 06:35:15 +0330

    Update chepsetProductStore.ts

--- src/core/store/chepsetProductStore.ts
+++ src/core/store/chepsetProductStore.ts
@@ -1,1 +1,37 @@
 
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { productService } from '@/services/product.service'
+import type { Product } from '@/types/product.types'
+
+export const usechepsettProductStore = defineStore('latestProducts', () => {
+  const products = ref<Product[]>([])
+  const loading = ref(false)
+  const error = ref<string | null>(null)
+
+  async function fetchChepsetProducts() {
+    loading.value = true
+    error.value = null
+
+    try {
+      const res = await productService.getChesetProducts()
+
+      if (Array.isArray(res)) {
+        products.value = res
+      } else {
+        products.value = res.data ?? res.results ?? []
+      }
+    } catch (err: any) {
+      error.value = err.message || 'خطا در دریافت جدیدترین محصولات'
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    products,
+    loading,
+    error,
+    fetchChepsetProducts,
+  }
+})

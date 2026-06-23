cfbd7dd8053afee436641b0dcc4e244c20e6048f
flip64 <65002468+flip64@users.noreply.github.com>
2026-06-23 18:25:48 +0330

    Update CheapestProductsCarouel.vue

--- src/components/home/CheapestProductsCarouel.vue
+++ src/components/home/CheapestProductsCarouel.vue
@@ -120,7 +120,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue"
 import { useRouter } from "vue-router"
-import { useChepsetProductStore } from "@/core/store/chepsetProductStore"
+import { cheapestProductStore } from "@/core/store/cheapstProductStore"
 
 const router = useRouter()
 const slider = ref<HTMLElement | null>(null)

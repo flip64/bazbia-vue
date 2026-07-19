98d5dc17f97543508ce8d803f8c9e52bc33eaa31
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-19 07:30:55 +0330

    Update FeaturedProductsSwiper.vue

--- src/components/home/FeaturedProductsSwiper.vue
+++ src/components/home/FeaturedProductsSwiper.vue
@@ -19,9 +19,7 @@
           <h2 class="font-bold text-lg text-green-800">
             محصولات ویژه
           </h2>
-          <span class="text-xs text-gray-500">
-  تعداد: {{ products.length }}
-</span>
+          
 
           <!-- 🔴 Only Today Badge -->
           <span class="bg-red-500 text-white text-xs px-2 py-1 rounded-full animate-pulse">

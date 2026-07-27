37dbd992dcad2e8891e236a5a843b3966a7c52e0
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-27 12:54:11 +0330

    Update address.service.ts

--- src/services/address.service.ts
+++ src/services/address.service.ts
@@ -1,1 +1,60 @@
+import api from '@/core/api'
 
+import type {
+  CustomerAddress,
+  CreateCustomerAddressPayload,
+  UpdateCustomerAddressPayload
+} from '@/types/address.types'
+
+const BASE_URL = '/customers/addresses/'
+
+export const addressService = {
+  async getAddresses(): Promise<CustomerAddress[]> {
+    const response = await api.get<CustomerAddress[]>(
+      BASE_URL
+    )
+
+    return response.data
+  },
+
+  async getAddress(
+    id: number
+  ): Promise<CustomerAddress> {
+    const response = await api.get<CustomerAddress>(
+      `${BASE_URL}${id}/`
+    )
+
+    return response.data
+  },
+
+  async createAddress(
+    payload: CreateCustomerAddressPayload
+  ): Promise<CustomerAddress> {
+    const response = await api.post<CustomerAddress>(
+      BASE_URL,
+      payload
+    )
+
+    return response.data
+  },
+
+  async updateAddress(
+    id: number,
+    payload: UpdateCustomerAddressPayload
+  ): Promise<CustomerAddress> {
+    const response = await api.patch<CustomerAddress>(
+      `${BASE_URL}${id}/`,
+      payload
+    )
+
+    return response.data
+  },
+
+  async deleteAddress(
+    id: number
+  ): Promise<void> {
+    await api.delete(
+      `${BASE_URL}${id}/`
+    )
+  }
+}

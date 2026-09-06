ef8588159d2811223d6a8056f177a397a646c1d9
flip64 <65002468+flip64@users.noreply.github.com>
2026-09-06 13:44:23 +0330

    Update DefaultLayout.vue

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,27 +1,67 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-gray-50 rtl">
-
+  <div
+    class="min-h-screen flex flex-col bg-gray-50 rtl"
+  >
     <!-- Global Loading -->
     <div
       v-if="loading"
-      class="fixed inset-0 bg-black/40 z-50 flex items-center justify-center"
+      class="
+        fixed
+        inset-0
+        bg-black/40
+        z-50
+        flex
+        items-center
+        justify-center
+      "
     >
-      <div class="w-14 h-14 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+      <div
+        class="
+          w-14
+          h-14
+          border-4
+          border-white
+          border-t-transparent
+          rounded-full
+          animate-spin
+        "
+      ></div>
     </div>
+
 
     <!-- Header -->
     <Header />
 
+
     <!-- Page Content -->
-    <main class="flex-1 container mx-auto px-4 py-6">
+    <main
+      class="
+        flex-1
+        container
+        mx-auto
+        px-4
+        py-6
+        pb-24
+        md:pb-6
+      "
+    >
       <router-view />
     </main>
+
 
     <!-- Footer -->
     <Footer />
 
+
+    <!-- Mobile Bottom Navigation -->
+    <MobileBottomNav />
+
+
     <!-- Toasts -->
-    <ToastContainer :toasts="toasts" />
+    <ToastContainer
+      :toasts="toasts"
+    />
+
 
     <!-- Modal -->
     <Modal
@@ -34,19 +74,32 @@
 </template>
 
 <script setup lang="ts">
-import Header from "@/components/layout/header/Header.vue"
-import Footer from "@/components/layout/Footer.vue"
-import ToastContainer from "@/components/ui/ToastContainer.vue"
-import Modal from "@/components/ui/Modal.vue"
+import Header from '@/components/layout/header/Header.vue'
+import Footer from '@/components/layout/Footer.vue'
+import MobileBottomNav from '@/components/layout/MobileBottomNav.vue'
 
-import { useLoading } from "@/composables/useLoading"
-import { useToast } from "@/composables/useToast"
-import { useModal } from "@/composables/useModal"
+import ToastContainer from '@/components/ui/ToastContainer.vue'
+import Modal from '@/components/ui/Modal.vue'
+
+import { useLoading } from '@/composables/useLoading'
+import { useToast } from '@/composables/useToast'
+import { useModal } from '@/composables/useModal'
+
 
 /* ---------------- GLOBAL STATE ---------------- */
 
-const { loading } = useLoading()
-const { toasts } = useToast()
+const {
+  loading
+} = useLoading()
 
-const { modal, closeModal } = useModal()
+
+const {
+  toasts
+} = useToast()
+
+
+const {
+  modal,
+  closeModal
+} = useModal()
 </script>

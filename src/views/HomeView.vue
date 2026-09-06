e311fc27ff86a589a94498f02cc60e1b3d07b8d5
flip64 <65002468+flip64@users.noreply.github.com>
2026-09-06 06:07:58 +0330

    Update text in HomeView.vue for clarity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
     class="text-2xl font-bold leading-relaxed
            text-gray-900 md:text-3xl"
   >
-    بازبیا؛ انتخابی که آنرا تکرار خواهید کرد 
+    بازبیا؛ انتخابی که دوباره به آن برمی‌گردید
   </h1>
 
   <p
